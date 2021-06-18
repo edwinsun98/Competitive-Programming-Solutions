43dfed526539c6fda364d10472619490c128dbc9
edwinsun98 <57502912+edwinsun98@users.noreply.github.com>
2021-06-17 21:17:38 -0400

    Create ABC180_A.cpp

---
+++ Atcoder/ABC180_A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define p push
+#define ll long long
+#define lld long long double
+#define f first
+#define s second
+#define all(vec) begin(vec), end(vec)
+#define pii pair<int,int>
+#define pli pair<ll,int>
+#define pil pair<int,ll>
+#define pll pair<ll,ll>
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+using namespace std;
+ 
+ 
+ 
+ 
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//    freopen("maxcross.in", "r", stdin);
+//    freopen("maxcross.out", "w", stdout);
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << n-a+b << '\n';
+    return 0;
+}
