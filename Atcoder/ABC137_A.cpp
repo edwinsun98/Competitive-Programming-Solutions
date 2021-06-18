3186da95747ffc5515b0586cc577dde71f0c3bc7
edwinsun98 <57502912+edwinsun98@users.noreply.github.com>
2021-06-17 21:34:07 -0400

    Create ABC137_A.cpp

---
+++ Atcoder/ABC137_A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define f first
+#define s second
+#define all(vec) begin(vec), end(vec)
+#define pf push_front
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mk make_pair
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ll,int> pli;
+ 
+ 
+ 
+void solve(){
+    int a, b;
+    cin >> a >> b;
+    cout << max({a+b, a-b, a*b}) << '\n';
+}
+ 
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//    freopen("input.in", "r", stdin);
+//    freopen("output.out", "w", stdout);
+    int tc = 1;
+//    cin >> tc;
+    while(tc--){
+        solve();
+    }
+}
