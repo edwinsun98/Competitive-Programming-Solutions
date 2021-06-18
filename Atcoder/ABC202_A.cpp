648f8e6521255774cec908ded87c49d88a36cc88
edwinsun98 <57502912+edwinsun98@users.noreply.github.com>
2021-06-17 21:09:00 -0400

    Create ABC202_A.cpp

---
+++ Atcoder/ABC202_A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define all(vec) begin(vec), end(vec)
+#define pf push_front
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mk make_pair
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ll,int> pli;
+ 
+ 
+void solve(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << 21-a-b-c << '\n';
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t = 1;
+//    cin >> t;
+    while(t--){
+        solve();
+    }
+}
