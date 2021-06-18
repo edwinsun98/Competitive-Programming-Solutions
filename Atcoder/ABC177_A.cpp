e712137d9acffd7ca78afb2c67c6912b95065ed3
edwinsun98 <57502912+edwinsun98@users.noreply.github.com>
2021-06-17 21:20:44 -0400

    Create ABC177_A.cpp

---
+++ Atcoder/ABC177_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long
+#define f first
+#define s second
+#define endl '\n'
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ll d,t, s; cin >> d >> t >> s;
+    if(t*s >= d)cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
