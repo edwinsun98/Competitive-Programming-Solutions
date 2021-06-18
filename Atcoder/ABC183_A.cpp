5fd22a82db20d397adbc394f7ff7ecf4c0971353
edwinsun98 <57502912+edwinsun98@users.noreply.github.com>
2021-06-17 21:11:29 -0400

    Create ABC183_A.cpp

---
+++ Atcoder/ABC183_A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define p push
+#define f first
+#define s second
+#define all(vec) begin(vec), end(vec)
+#define pii pair<int,int>
+#define pli pair<ll,int>
+#define pil pair<int,ll>
+#define pll pair<ll,ll>
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+typedef long long ll;
+using namespace std;
+ 
+ 
+ 
+ 
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int x;
+    cin >> x;
+    if(x >= 0)cout << x<< '\n';
+    else cout << 0 << '\n';
+}
