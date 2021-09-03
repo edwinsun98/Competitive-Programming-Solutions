b039e02b1c8aaf623640f7a6b69ccd86cdcc456b
Edwin Sun <57502912+edwinsun98@users.noreply.github.com>
2021-09-02 21:45:57 -0400

    Create A. Piles With Stones.cpp

---
+++ Codeforces/Codeforces Round #500 (Div. 2)/A. Piles With Stones.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define f first
+#define s second
+#define all(vec) begin(vec), end(vec)
+#define pf push_front
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mk make_pair
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ll,int> pli;
+
+void solve(){
+    int n;
+    cin >> n;
+    int sa = 0, sb = 0;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        sa += x;
+    }
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        sb += x;
+    }
+    cout << (sa >= sb ? "Yes" : "No") << '\n';
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//    freopen("input.in", "r", stdin);
+//    freopen("output.out", "w", stdout);
+    int tc = 1;
+//    cin >> tc;
+    while(tc--){
+        solve();
+    }
+}
