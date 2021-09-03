0bc23fc2073d56708c6de1185ff5dcf0de75d133
Edwin Sun <57502912+edwinsun98@users.noreply.github.com>
2021-09-02 21:53:41 -0400

    Create B. World Cup.cpp

---
+++ Codeforces/Codeforces Round #492 (Div. 2)/B. World Cup.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define f first
+#define s second
+#define all(vec) begin(vec), end(vec)
+#define pf push_front
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mk make_pair
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ll,int> pli;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    ll ans = 1e18, ret;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        if(i+((a[i]-i+n-1)/n)*n < ans){
+            ans = i+((a[i]-i+n-1)/n)*n;
+            ret = i;
+        }
+        ans = min(ans, i+((a[i]-i+n-1)/n)*n);
+    }
+    cout << ret+1 << '\n';
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//    freopen("input.in", "r", stdin);
+//    freopen("output.out", "w", stdout);
+    int tc = 1;
+//    cin >> tc;
+    while(tc--){
+        solve();
+    }
+}
